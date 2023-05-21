48d013bc57da9d0ed383b4b49f7c6f8de36d29aa
Yerke <200107109@stu.sdu.edu.kz>
2023-05-21 11:23:18 +0600

    merged

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-// Get the table element
-var table = document.getElementById("myTable");
+// Make an HTTP request to retrieve the JSON data
+fetch('localhost:9090/shortestpath')
+    .then(response => response.json())
+    .then(data => {
+        // Process the received JSON data here
+        console.log(data);
+        // Access specific properties of the JSON object
+        var start = data.start;
+        var end = data.end;
+        var path = data.path;
+        var distance = data.distance;
+        var index = data.index;
 
-// Specify the row and column indices you want to change the color for
-var rowIndex = 1; // Index of the row (starting from 0)
-var columnIndex = 2; // Index of the column (starting from 0)
+        var table = document.getElementById("myTable");
 
-// Get the cell in the specified row and column
-var cell = table.rows[rowIndex].cells[columnIndex];
+        // Specify the row and column indices you want to change the color for
+        var rowIndex = 2; // Index of the row (starting from 0)
+        var columnIndex = 6; // Index of the column (starting from 0)
 
-// Set the color of the cell
-cell.style.backgroundColor = "#77e162";
-cell.style.fontWeight = 'bold';
+        // Get the cell in the specified row and column
+        var cell = table.rows[rowIndex].cells[columnIndex];
+
+        // Set the color of the cell
+        cell.style.backgroundColor = "#77e162";
+        cell.style.fontWeight = 'bold';
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the request
+        console.log('Error:', error);
+    });
